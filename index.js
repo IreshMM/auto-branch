e05868ee525fd2710cd180089741296d97046516
Iresh Dissanayaka <socialexpz1@gmail.com>
2019-12-30 15:03:32 +0530

    implementation

--- index.js
+++ index.js
@@ -3,17 +3,97 @@
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.on('repository.created', async context => {
+    await createBranches(context);
+    await updateBranchProtections(context);
   })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  async function updateBranchProtections(context) {
+    await protectBranch(context, 'dev_protected');
+    await protectBranch(context, 'sit_protected');
+  }
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  async function protectBranch(context, branch) {
+    const owner = await repoOwnerOf(context);
+    const repo = await repoNameOf(context);
+    const required_status_checks = null;
+    const enforce_admins = null;
+    const required_pull_request_reviews = {
+      required_approving_review_count: 1
+    };
+    const restrictions = null;
+
+    const params = {
+      owner,
+      repo,
+      branch,
+      required_status_checks,
+      enforce_admins,
+      required_pull_request_reviews,
+      restrictions,
+      mediaType : {
+        previews: ["luke-cage-preview"]
+      }
+    };
+
+    await context.github.repos.updateBranchProtection(params);
+  }
+
+  async function getMasterBranchSha(context) {
+    return (await getRef(context, 'master')).object.sha;
+  }
+
+  async function getRef(context, refName) {
+    const owner = await repoOwnerOf(context);
+    const repo =  await repoNameOf(context);
+    const ref = `heads/${refName}`
+
+    const params = {
+      owner,
+      repo,
+      ref
+    };
+
+    try {
+      return (await context.github.git.getRef(params)).data;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  async function createBranches(context) {
+    masterSha = await getMasterBranchSha(context);
+
+    await createBranch(context, masterSha, 'dev_protected');
+    await createBranch(context, masterSha, 'sit_protected');
+  }
+
+  async function createBranch(context, masterSha, branchName) {
+    if (await branchExists(context, branchName)) return;
+
+    const params = {
+      owner: await repoOwnerOf(context),
+      repo: await repoNameOf(context),
+      ref: `refs/heads/${branchName}`,
+      sha: masterSha
+    };
+
+
+    context.github.git.createRef(params).then(console.log);
+
+  }
+
+  // Helper functions
+  async function branchExists(context, branchName) {
+    return (await getRef(context, branchName)) ? true : false;
+  }
+
+  async function repoNameOf(context) {
+    return context.payload.repository.name;
+  }
+
+  async function repoOwnerOf(context) {
+    return context.payload.repository.owner.login;
+  }
 }
